6e7d85eeec853bb43963a71de4ebc7e9fdad66a8
Simon Tatham <anakin@pobox.com>
2010-04-12 17:24:10 +0100

    Add a 'clean' option to autostuff.sh, and some cleanups.

--- autostuff.sh
+++ autostuff.sh
@@ -1,2 +1,8 @@
 #!/bin/sh 
-aclocal && autoconf && autoheader && automake -a --foreign
+
+if test "x$1" = "xclean"; then
+  rm -f Makefile.in config.h.in configure depcomp install-sh missing ylwrap aclocal.m4
+else
+  aclocal && autoconf && autoheader && automake -a --foreign
+fi
+rm -rf autom4te.cache
