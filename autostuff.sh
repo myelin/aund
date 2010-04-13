579921bd25f265cee7cfeb7c37881e9b4a73330d
Ben Harris <bjh21@bjh21.me.uk>
2010-04-13 22:08:59 +0100

    Move "foreign" option into AM_INIT_AUTOMAKE so that when autoreconf runs automake it'll run it in the right mode.

--- autostuff.sh
+++ autostuff.sh
@@ -3,6 +3,6 @@
 if test "x$1" = "xclean"; then
   rm -f Makefile.in config.h.in configure depcomp install-sh missing ylwrap aclocal.m4
 else
-  aclocal && autoconf && autoheader && automake -a --foreign
+  aclocal && autoconf && autoheader && automake -a
 fi
 rm -rf autom4te.cache
